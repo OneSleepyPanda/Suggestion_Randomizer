3e2cc1427c95ea211cadfb01b2f5203ea70e441e
OneSleepyPanda <louisdtobin@outlook.com>
2024-08-02 10:19:16 -0400

    added better_profanity module and content filter to prompts before they're stored

--- responses.py
+++ responses.py
@@ -1,8 +1,9 @@
 from random import choice, randint
 import suggestions
+from better_profanity import profanity
 
 
-
+# Recreates the prompt list with the updated content
 def regen_prompts(prompts):
     prompt_list = prompts
     with open('suggestions.py', 'w') as f:
@@ -13,7 +14,10 @@ def regen_prompts(prompts):
         print('Prompt list has been regenerated')
         return
 
+
+#processes User Entries
 def get_response(user_input: str) -> str:
+        #Cleanup of user input
         lowered: str = user_input.lower()
         trimmed: str = lowered.split(' ', 1)
         trigger: str = trimmed[0]
@@ -21,18 +25,32 @@ def get_response(user_input: str) -> str:
         prompt_list = suggestions.prompt_list
         
         print(trigger)
+        
+        #processes input
         if trimmed == '':
             return 'Well, you\'re awfully silent...'
+        
+        #!get-prompt selects a random prompt from the list and removes it, if no prompts are stored generates one from chat gpt
         elif trigger == '!get-prompt':
-            print('getting prompt')
-            chosen_prompt = choice(prompt_list)
-            print(chosen_prompt)
-            prompt_list.remove(chosen_prompt)
-            regen_prompts(prompt_list)
-            return f'The art prompt of the day is: {chosen_prompt}'
+            if prompt_list:
+                print('getting prompt')
+                print(len(prompt_list))
+                chosen_prompt = choice(prompt_list)
+                print(chosen_prompt)
+                prompt_list.remove(chosen_prompt)
+                regen_prompts(prompt_list)
+                return f'The art prompt of the day is: {chosen_prompt}'
+            else:
+                return f'The prompt list is empty! Suggest some ideas you filthy animals!'
+                
+        
+        #adds user provided prompt to the list
         else:
-            print(f'adding {user_prompt} to prompt list')
-            prompt_list.append(user_prompt)
-            print(prompt_list)
-            regen_prompts(prompt_list)
-            return f'Your Suggestion Has Been Added!'
+            print(f'checking prompt for profanity')
+            if not profanity.contains_profanity(user_prompt):
+                prompt_list.append(user_prompt)
+                print(prompt_list)
+                regen_prompts(prompt_list)
+                return f'Your Suggestion Has Been Added!'
+            else:
+                return 'Your suggestion did not pass our profanity filter, clean up your language and try submitting it again!'
